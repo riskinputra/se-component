b60fdb113351dd3ed505180254b4e71b95c3587b
riskinputra <riski.n.putra@gmail.com>
2020-10-12 15:50:42 +0700

    initial commit

---
+++ packages/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" src="./styles/font/font-family.scss"></style>
+<style lang="scss" src="./styles/icon/se-font.min.scss"></style>
+<style lang="scss">
+@import './styles/index.scss';
+</style>
---
+++ packages/frontend/src/components/SEButton/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    type="button"
+    :class="classes"
+    :style="style"
+    @click="onClick"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SEButton',
+  label: {
+    type: String,
+    required: true,
+    default: 'Button'
+  },
+  color: {
+    type: Boolean,
+    default: 'default',
+    validator: function (value) {
+      return ['primary', 'secondary', 'accent', 'default'].indexOf(value) !== -1
+    }
+  },
+  size: {
+    type: String,
+    default: 'medium',
+    validator: function (value) {
+      return ['small', 'medium', 'large'].indexOf(value) !== -1
+    }
+  }
+}
+</script>
